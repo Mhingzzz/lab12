812ae5d08b0a9f220207b8554401999dfb6e7eaf
mm <nathaphong_p@cmu.ac.th>
2021-12-21 18:14:12 +0700

    ไม่ไหวละ

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,23 @@
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    int i = 0;
+    while (i < 3)
+    {
+        cin.get();
+        i++;
+    }
+    cout <<"You will get "<<grade[rand()%10] << " in this 261102." ;
+    return 0;
+}
+
